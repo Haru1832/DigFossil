7a091da64722cae5a6d99bd38106788df05d3fd7
gomadare <minecrafter1832810@gmail.com>
2022-01-29 16:18:43 +0900

    FirstCommit

---
+++ Board.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    
+    // [SerializeField]
+    // private Game m_Game;
+    [SerializeField]
+    private int m_Columns;
+    [SerializeField]
+    private int m_Rows;
+
+    [SerializeField]
+    private GameObject m_TilePrefab;
+
+    //Not the fastest collection but easy to use.
+    private List<Tile> m_TilesList;
+
+    public Tile[,] boardTileArray2d
+    {
+        get { return GetTilesAs2dArray(); }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    private void GenerateBoard()
+    {
+        m_TilesList = new List<Tile>();
+
+        for (int i = 0; i < m_Columns; i++)
+        {	
+            for (int j = 0; j < m_Rows+1; j++)
+            {
+                float xPos = -(m_Columns/2f) + i + 0.5f;
+                float yPos = -(m_Rows/2f) + j ;
+                
+                    var newTileObject = GameObject.Instantiate(m_TilePrefab, new Vector3(xPos, 0, yPos), Quaternion.Euler(90,0,0));
+                    var newTile = newTileObject.GetComponent<Tile>();
+
+                    if (newTile != null)
+                    {
+                        int random = Random.Range(0, 6);
+                        newTile.Initialize(i, j,random);
+                        m_TilesList.Add(newTile);
+                    }
+            }
+        }
+    }
+    
+    private Tile[,] GetTilesAs2dArray()
+    {
+        Tile[,] boardTileArray2d = new Tile[m_Columns, m_Rows];
+
+        for (int i = 0; i < m_TilesList.Count; i++)
+        {
+            var t = m_TilesList[i];
+            boardTileArray2d[t.Column, t.Row] = t;
+        }
+
+        return boardTileArray2d;
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int Column { get { return m_Column; } }
+    public int Row { get { return m_Row; } }
+    public int HP { get { return m_HP; } }
+    public bool isChecked;
+
+    [SerializeField] private GameObject[] tiles;
+
+    [SerializeField] private GameObject chooseTile;
+
+    private ChangeTileAlpha _changeTile;
+
+    //[SerializeField]
+    private int m_Column;
+    //[SerializeField]
+    private int m_Row;
+
+    private int m_HP;
+
+    private void Start()
+    {
+        _changeTile = chooseTile.GetComponent<ChangeTileAlpha>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(m_Column+":"+m_Row);
+    }
+
+    private void OnMouseOver()
+    {
+        // Debug.Log("MouseOver");
+        chooseTile.SetActive(true);
+        StartCoroutine(_changeTile.ColorCoroutine());
+    }
+    
+    private void OnMouseExit()
+    {
+        chooseTile.SetActive(false);
+        StopCoroutine(_changeTile.ColorCoroutine());
+    }
+
+    public void Initialize(int columnIndex, int rowIndex,int HPIndex)
+    {
+        m_Column = columnIndex;
+        m_Row = rowIndex;
+        m_HP = HPIndex;
+
+        
+        UpdateTile();
+    }
+
+    private void UpdateTile()
+    {
+        GameObject targetTile = GetNumberTile();
+        foreach (var tile in tiles)
+        {
+            tile.SetActive(false);
+        }
+        targetTile.SetActive(true);
+    }
+
+    private GameObject GetNumberTile()
+    {
+        return tiles[HP];
+    }
+
+}
