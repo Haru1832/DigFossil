61a2c8b79d8b9a52f55b7cf624422e9e353d6734
gomadare <minecrafter1832810@gmail.com>
2022-03-16 20:51:05 +0900

    View仮完成

--- NewScripts/View/BoardView.cs
+++ NewScripts/View/BoardView.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class BoardView : MonoBehaviour
 {
+    public GameObject PanelPrefab;
+
+    [SerializeField] private GameObject parentObject;
+    
+    private BoardPresenter _boardPresenter;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _boardPresenter = new BoardPresenter();
+        _boardPresenter.panels.ObserveAdd().Subscribe(x =>
+        {
+            InstatiatePanel(x.Value);
+        });
+
+        _boardPresenter.GenerateInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InstatiatePanel(PanelPresenter objValue)
     {
+        Debug.Log("Instantiate");
+        var panelObj = Instantiate(PanelPrefab, parentObject.transform, true);
+        panelObj.GetComponent<PanelView>().Init(objValue);
+    }
+
+    [SerializeField] private Material empty;
+    static Material Empty;
+
+    [SerializeField] private Material material1;
+    static Material Material1;
+    
+    [SerializeField] private Material material2;
+    static Material Material2;
+    
+    [SerializeField] private Material material3;
+    static Material Material3;
+    
+    [SerializeField] private Material material4;
+    static Material Material4;
+    
+    [SerializeField] private Material material5;
+    static Material Material5;
+    
+    
+
+
+    public static Material GetMaterial(int HPValue)
+    {
+        return HPValue switch
+        {
+            0 => Empty,
+            1 => Material1,
+            2 => Material2,
+            3 => Material3,
+            4 => Material4,
+            5 => Material5,
+            _ => null
+        };
+    }
+
+    public static Vector2 GetPanelPosition(int x,int y)
+    {
+        float xPos = -(Model.Width/2f) + x + 0.5f;
+        float yPos = -(Model.Height/2f) + y +0.5f;
         
+        return new Vector2(xPos,yPos);
     }
 }
--- NewScripts/View/PanelView.cs
+++ NewScripts/View/PanelView.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class PanelView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private MeshRenderer _renderer;
+   public void Init(PanelPresenter presenter)
+   {
+      SetMaterial(presenter.Panel.Value.panelHP);
+      SetPosition(presenter.Panel.Value.x,presenter.Panel.Value.y);
+      presenter.Panel.Subscribe(x => { SetMaterial(x.panelHP); })
+         .AddTo(this);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   void SetPosition(int x,int y)
+   {
+      Vector2 panelPosition = BoardView.GetPanelPosition(x, y);
+      transform.position = panelPosition;
+   }
+
+   void SetMaterial(int HPValue)
+   {
+      _renderer = GetComponent<MeshRenderer>();
+      Material material = BoardView.GetMaterial(HPValue);
+      _renderer.material = material;
+   }
 }
